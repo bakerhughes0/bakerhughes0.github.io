27498aea20e129935739336a89020b22da0d884d
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2022-09-17 13:57:54 +0200

    Updated on 2022-09-17 13:57:54

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2022,8,17,12,58,05);
+timestamp = new Date(2022,8,17,13,57,50);
 data = [
 ['0205',1,2,31590731,'0020. TOURNER','TIGE DE RESSORT',0.75,'',38],
 ['0205',1,2,31587868,'0020. TOURNER','TIGE DE RESSORT',2,'',38],
