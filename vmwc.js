12a72a9be4bda570413629d51dbf33beaab84e2d
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2022-10-02 11:58:08 +0200

    Updated on 2022-10-02 11:58:07

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2022,9,02,10,58,10);
+timestamp = new Date(2022,9,02,11,58,05);
 data = [
 ['0205',1,2,31630824,'0030. TOURNER (EBAUCHE)','SIEGE CFX 4" FACT 1',0.71,'',32],
 ['0205',1,2,31593861,'0010. TOURNER','PLAQUE DE DIAPHRAGME',4,'',32],
