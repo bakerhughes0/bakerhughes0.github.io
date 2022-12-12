d1170a158a1a2a3e270f749033f2a888260ae4de
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2022-12-13 00:55:27 +0100

    Updated on 2022-12-13 00:55:26

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2022,11,13,00,50,20);
+timestamp = new Date(2022,11,13,00,55,22);
 data = [
 ['0205',1,2,31593861,'0010. TOURNER','PLAQUE DE DIAPHRAGME',4,'',104],
 ['0205',1,2,31762320,'0010. TOURNER','PLAQUE DE DIAPHRAGME',1.3,'',12],
