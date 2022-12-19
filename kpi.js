78cc90af771f0a722cfb904320efd03f947b90e8
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2022-12-19 15:00:33 +0100

    Updated on 2022-12-19 15:00:33

--- kpi.js
+++ kpi.js
@@ -1,1 +1,1 @@
-data = {'wo_n_total':1894,'wo_h_total':1653,'wo_n_late_vs_plan':422,'wo_h_late_vs_plan':344,'wo_mach_n_total':832,'wo_mach_h_total':1382,'wo_mach_n_late_vs_plan':92,'wo_mach_h_late_vs_plan':265,'q_name':'22Q4','q_perc':0.87,'q_workdaysleft':9}
+data = {'wo_n_total':1885,'wo_h_total':1653,'wo_n_late_vs_plan':387,'wo_h_late_vs_plan':289,'wo_mach_n_total':827,'wo_mach_h_total':1381,'wo_mach_n_late_vs_plan':81,'wo_mach_h_late_vs_plan':229,'q_name':'22Q4','q_perc':0.87,'q_workdaysleft':9}
