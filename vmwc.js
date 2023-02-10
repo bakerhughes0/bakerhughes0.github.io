099158722fc1d1b3644510e3b426fbf8e6dee1a3
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-02-10 19:20:48 +0100

    Updated on 2023-02-10 19:20:48

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,1,10,19,15,38);
+timestamp = new Date(2023,1,10,19,20,42);
 data = [
 ['0205',1,2,31814771,'0010. TOURNER','PLAQUE DE DIAPHRAGME',0.4,'',32],
 ['0205',1,2,31777788,'0020. TOURNER','SPACER TUBE 35K 1.5-2',1,'',52],
