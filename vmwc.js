5bdbda622a66cf0f724790f12218fb87ad0f4869
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-02-22 23:35:53 +0100

    Updated on 2023-02-22 23:35:52

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,1,22,23,30,47);
+timestamp = new Date(2023,1,22,23,35,45);
 data = [
 ['0205',1,2,31814771,'0010. TOURNER','4 x PLAQUE DE DIAPHRAGME',0.4,'',44],
 ['0205',1,3,31811481,'0010. TOURNER','1 x CLAPET CV 0,25 $ 0,10',0.75,'',0],
