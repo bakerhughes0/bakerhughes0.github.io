f36842a7acf2a973e54059932588d4d97e62a696
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-03-30 12:40:44 +0200

    Updated on 2023-03-30 12:40:43

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,2,30,12,35,35);
+timestamp = new Date(2023,2,30,12,40,37);
 data = [
 ['0205',1,3,31760869,'0010. TOURNER (GORGE)','1 x ARBRE (GARLOCK 2"-5")REUSINAGE',0.15,'',0],
 ['0205',1,3,31796573,'0020. TOURNER GOUPILLE','1 x GOUPILLE',0.2,'',0],
