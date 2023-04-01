533793978f3da2b65c5b813bdba6f587909e3e78
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-01 23:10:32 +0200

    Updated on 2023-04-01 23:10:31

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,01,23,05,34);
+timestamp = new Date(2023,3,01,23,10,26);
 data = [
 ['0205',1,2,31795321,'0060. TOURNER  (FINITION)','1 x CAGE USINAGE  21K 1.5-2" FULL AREA',0.5,'',1],
 ['0205',,2,31864579,'0010. TOURNER','3 x ENS (BOITE+RONDELLES) 1"-2"CFX',0.6,'',18],
