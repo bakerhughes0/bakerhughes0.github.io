b57b5ab7e9e88c0c041e8be42faef2b8fc946938
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-12 20:10:57 +0200

    Updated on 2023-04-12 20:10:57

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,12,20,05,53);
+timestamp = new Date(2023,3,12,20,10,47);
 data = [
 ['0205',1,2,31895126,'0020. TOURNER','12 x MANCHON CHAPE PLONGEUR PLIABLE',6,'',34,''],
 ['0205',1,2,31895127,'0020. TOURNER','2 x SOMMET DE PLONGEUR PLIABLE',0.3,'',35,''],
