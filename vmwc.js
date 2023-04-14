e6a5963c95ce61284813d1fdec9fd0533497d424
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-14 03:35:32 +0200

    Updated on 2023-04-14 03:35:32

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,14,03,30,24);
+timestamp = new Date(2023,3,14,03,35,26);
 data = [
 ['0205',1,2,31895127,'0020. TOURNER','2 x SOMMET DE PLONGEUR PLIABLE',0.3,'',37,''],
 ['0205',,2,31604522,'0020. TOURNER (EB AV STEL)','1 x SIEGE PINCE 21K 1" A 2" CV 6',0.5,'975711-2000 GE Steam Power Systems',3,''],
