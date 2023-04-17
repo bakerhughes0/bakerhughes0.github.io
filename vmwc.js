f0d09da1a658775e0d675132a6f5986384799bee
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-17 02:15:35 +0200

    Updated on 2023-04-17 02:15:34

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,17,02,10,23);
+timestamp = new Date(2023,3,17,02,15,31);
 data = [
 ['0205',,2,31604522,'0020. TOURNER (EB AV STEL)','1 x ',0.5,'975711-2000 GE Steam Power Systems',6,''],
 ['0205',,2,31604523,'0020. TOURNER (EB AV STEL)','1 x ',0.5,'975711-3000 GE Steam Power Systems',6,''],
