8eccab14025dc6889304d8a48ca0682f83dddc1d
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-18 03:30:47 +0200

    Updated on 2023-04-18 03:30:46

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,18,03,25,21);
+timestamp = new Date(2023,3,18,03,30,39);
 data = [
 ['0205',,2,31944323,'0050. FINITION SIEGE','1 x SIEGE FAC.0,6 CMF 1"1/2',1.25,'',1,''],
 ['0205',,2,31604522,'0020. TOURNER (EB AV STEL)','1 x SIEGE PINCE 21K 1" A 2" CV 6',0.5,'975711-2000 GE Steam Power Systems',7,''],
