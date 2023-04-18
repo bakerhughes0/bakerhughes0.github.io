0d4321aa23ad0ec3a5c1b8c5478ef8834e5f6b0a
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-19 01:20:33 +0200

    Updated on 2023-04-19 01:20:32

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,19,01,15,26);
+timestamp = new Date(2023,3,19,01,20,26);
 data = [
 ['0205',1,2,31954548,'0020. TOURNER FLASQUE INFERIEUR DE PLONG','2 x FOND DE PLONGEUR DIA 22 E=4 22L',0.5,'',1,''],
 ['0205',,2,31604522,'0020. TOURNER (EB AV STEL)','1 x SIEGE PINCE 21K 1" A 2" CV 6',0.5,'975711-2000 GE Steam Power Systems',8,''],
