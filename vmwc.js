e70949befc823d5277bd49e0317039284910b309
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-04-24 20:30:52 +0200

    Updated on 2023-04-24 20:30:52

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,3,24,20,25,41);
+timestamp = new Date(2023,3,24,20,30,43);
 data = [
 ['0205',,1,31964672,'0070. TOURNER(FINITION #B)','1 x SIEGE 413.5/5.5/6.5/9.5 2"1500',2,'',0,''],
 ['0205',,3,31971488,'0020. TOURNER PACKING','2 x GRAIN DE PE 25.4 SPECIAL OUTILLAGE',3.5,'',0,''],
