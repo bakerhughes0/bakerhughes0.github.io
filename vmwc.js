aecae9f09730ff007da3293aa922ababa3417f55
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-06-14 23:35:36 +0200

    Updated on 2023-06-14 23:35:35

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,5,14,23,30,42);
+timestamp = new Date(2023,5,14,23,35,27);
 data = [
 ['0205',1,2,31878238,'0020. FRAPPER CODE MATIERE','2 x GRAIN DE PE',0.002,'',85,''],
 ['0205',1,2,32007753,'0020. TOURNER','8 x MANCHON CHAPE PLONGEUR PLIABLE',4,'',22,''],
