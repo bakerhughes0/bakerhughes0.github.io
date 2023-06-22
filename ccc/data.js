384589e4335a154d56af75a2b48632a89a3851df
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-06-22 15:21:17 +0200

    Updated on 2023-06-22 15:21:17

--- ccc/data.js
+++ ccc/data.js
@@ -1,2 +1,13 @@
-saf_pnt=[1,2,,,1,1,1,1,3,,,1,1,1,1,1,,,0,0,0,0,0,,,0,0,0,0,0];
-saf_pnt_txt=[,'Remarque pour essai',,,,,,,'Foulure du pied de Stephane Louvet',,,,,,,'Aujourd\'hui',,,,,,,,,,,,,,];
+1,2,,,1,1,1,1,3,,,1,1,1,1,1,,,0,0,0,0,0,,,0,0,0,0,0];
+,'Remarque pour essai',,,,,,,'Foulure du pied de Stephane Louvet',,,,,,,'Aujourd\'hui',,,,,,,,,,,,,,];
+saf_pnt=[0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+saf_pnt_txt=[,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
+
+0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
+
+0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
+
+0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
