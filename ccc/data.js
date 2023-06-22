32e8b3f225118d7184451091546cc28dbda163a8
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-06-22 15:23:15 +0200

    Updated on 2023-06-22 15:23:15

--- ccc/data.js
+++ ccc/data.js
@@ -1,13 +1,13 @@
-1,2,,,1,1,1,1,3,,,1,1,1,1,1,,,0,0,0,0,0,,,0,0,0,0,0];
-,'Remarque pour essai',,,,,,,'Foulure du pied de Stephane Louvet',,,,,,,'Aujourd\'hui',,,,,,,,,,,,,,];
+saf_usi=[1,2,,,1,1,1,1,3,,,1,1,1,1,1,,,0,0,0,0,0,,,0,0,0,0,0];
+saf_usi_txt=[,'Remarque pour essai',,,,,,,'Foulure du pied de Stephane Louvet',,,,,,,'Aujourd\'hui',,,,,,,,,,,,,,];
 saf_pnt=[0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
 saf_pnt_txt=[,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
 
-0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
-,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
+saf_pic=[0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+saf_pic_txt=[,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
 
-0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
-,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
+saf_std=[0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+saf_std_txt=[,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
 
-0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
-,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
+saf_con=[0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0,,,0,0,0,0,0];
+saf_con_txt=[,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,,'non-travaille','non-travaille',,,,,];
