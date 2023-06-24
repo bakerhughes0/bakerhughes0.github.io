0cd1c8ef515351508d04be1ce3844c54ba72c553
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-06-24 03:50:28 +0200

    Updated on 2023-06-24 03:50:28

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,5,24,03,45,27);
+timestamp = new Date(2023,5,24,03,50,22);
 data = [
 ['0205',1,2,31967993,'0020. TOURNER','8 x FOND PLONGEUR DIA 31x12',4,'',67,''],
 ['0205',,2,32055625,'0020. TOURNER','2 x FLASQUE INFERIEUR DE PLONGEUR',0.5,'',8,''],
