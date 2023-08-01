6a0212e28087b8ed85a87f2d7a010e17fd5dcb13
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-08-01 19:30:44 +0200

    Updated on 2023-08-01 19:30:44

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,7,01,19,25,34);
+timestamp = new Date(2023,7,01,19,30,35);
 data = [
 ['0205',1,2,31967993,'0020. TOURNER','8 x FOND PLONGEUR DIA 31x12',4,'',105,'L'],
 ['0205',1,1,32124071,'0020. TOURNER DOUILLE D\'EXTENSION TUBE T','1 x DOUILLE D\'EXTENSION',3,'',0,''],
