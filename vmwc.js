dcd5f2accae590cbc542dcb8370a0ae5416eb7ff
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-09-11 20:10:37 +0200

    Updated on 2023-09-11 20:10:36

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,8,11,20,05,38);
+timestamp = new Date(2023,8,11,20,10,28);
 data = [
 ['0205',1,2,31967993,'0020. TOURNER','8 x FOND PLONGEUR DIA 31x12',4,'',146,'L'],
 ['0205',1,2,32128320,'0040. TOURNER','3 x BAGUE DE SERRAGE',0.45,'',38,''],
