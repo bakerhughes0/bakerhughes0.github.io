317b9a7fcf6f3f08d8129c9f77fd7b1069dfb4d2
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-10-13 02:55:50 +0200

    Updated on 2023-10-13 02:55:49

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,9,13,02,50,57);
+timestamp = new Date(2023,9,13,02,55,46);
 data = [
 ['0205',1,2,31967993,'0020. TOURNER','8 x FOND PLONGEUR DIA 31x12',4,'',178,'L'],
 ['0205',1,2,32148445,'0040. TOURNER','1 x SIEGE CAMFLEX FACT 1',0.9,'',49,''],
