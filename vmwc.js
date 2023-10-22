24657597d48b5ff9e09f2caab5406a7493d00e32
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-10-23 01:55:48 +0200

    Updated on 2023-10-23 01:55:47

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,9,23,01,50,45);
+timestamp = new Date(2023,9,23,01,55,43);
 data = [
 ['0205',1,2,31967993,'0020. TOURNER','8 x FOND PLONGEUR DIA 31x12',4,'',188,'L'],
 ['0205',1,2,32114636,'0030. RELEVER NO DE COULEE','1 x SIEGE CV 30',0,'',89,''],
