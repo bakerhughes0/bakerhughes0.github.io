0d92ad2692b37a6d26afba7db306c09c908d6990
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2023-12-10 00:20:26 +0100

    Updated on 2023-12-10 00:20:26

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2023,11,10,00,15,24);
+timestamp = new Date(2023,11,10,00,20,22);
 data = [
 ['0205',1,2,32282018,'0030. TOURNER GRAIN DE PE...............','3 x GRAIN DE PRESSE-ETOUPE',2.25,'',3,''],
 ['0205',,2,32222523,'0010. TOURNER (REUSINAGE)','1 x BAGUE SERRAGE 35K 6"(REUSINAGE',0.2,'',55,''],
