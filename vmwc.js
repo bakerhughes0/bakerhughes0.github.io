a2a10a46d542d6c0cf6c3f570bc61317f10f5914
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-01-21 20:55:31 +0100

    Updated on 2024-01-21 20:55:30

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,0,21,20,50,29);
+timestamp = new Date(2024,0,21,20,55,26);
 data = [
 ['0205',,2,32339356,'0040. TOURNER','1 x PLUG 536H 2 600 500 HF',3.5,'',4,''],
 ['0205',,2,32364924,'0020.  TOURNER MANCHON','1 x MANCHON D\'ACCOUPLEMENT DE PLONGEUR 1',0.75,'',3,''],
