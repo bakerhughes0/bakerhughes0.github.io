41e465b1702ff6214f43fc354bb4f2c7223ed3af
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-01-26 00:36:02 +0100

    Updated on 2024-01-26 00:36:02

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,0,26,00,31,16);
+timestamp = new Date(2024,0,26,00,35,56);
 data = [
 ['0205',,2,32329184,'0020. TOURNER','1 x GUIDE SPECIAL',2,'',29,''],
 ['0205',,2,32301497,'0020. TOURNER GUIDE','16 x GUIDE',9.6,'',51,''],
