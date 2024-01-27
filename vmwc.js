b5683950e426d8f7fc598426e6509cbb2a048e3a
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-01-27 18:55:44 +0100

    Updated on 2024-01-27 18:55:43

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,0,27,18,50,41);
+timestamp = new Date(2024,0,27,18,55,39);
 data = [
 ['0205',,2,32301497,'0020. TOURNER GUIDE','16 x GUIDE',9.6,'',52,''],
 ['0205',,2,32349645,'0010. TOURNER','2 x ENTRETOISE(SIEGE VPK-BS CV<0.1',0.2,'',1,''],
