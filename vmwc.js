8aecf37dad3553fff677607e984fd521417bd348
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-02-03 19:15:46 +0100

    Updated on 2024-02-03 19:15:45

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,1,03,19,10,45);
+timestamp = new Date(2024,1,03,19,15,41);
 data = [
 ['0205',,2,32378596,'0020. TOURNER CONNEXION 3/4.............','1 x CONNECTION 3/4" SCH 160 L=69mm',1.75,'',3,''],
 ['0205',,2,32327735,'0020. TOURNER CLAPET 21000 3/4" 2"','1 x CLAPET 21000 1" CONTOUR LIN.',2,'',37,''],
