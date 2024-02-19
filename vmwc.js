743797c0a3bed3de1b075b14147bffe6851ce9d1
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-02-19 07:20:41 +0100

    Updated on 2024-02-19 07:20:40

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,1,19,07,15,28);
+timestamp = new Date(2024,1,19,07,20,35);
 data = [
 ['0205',1,2,32412292,'0040. FINITION','3 x SIEGE PINCE 21K 1" CV:12 2500',3.6,'',3,''],
 ['0205',,2,32327735,'0020. TOURNER CLAPET 21000 3/4" 2"','1 x CLAPET 21000 1" CONTOUR LIN.',2,'',53,''],
