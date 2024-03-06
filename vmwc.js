caec5bc93f708ec5f1dba8e6ca1d2b4d22c1db63
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-03-06 16:50:45 +0100

    Updated on 2024-03-06 16:50:43

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,2,06,16,45,31);
+timestamp = new Date(2024,2,06,16,50,36);
 data = [
 ['0205',1,3,32327735,'0020. TOURNER CLAPET 21000 3/4" 2"','1.000 x CLAPET 21000 1" CONTOUR LIN.',2,'',0,''],
 ['0205',,2,32387249,'0020. TOURNER','12 x ENTRETOISE VENT DE SABLE VPK',6.3,'',8,''],
