e65931b5bd142d43903520052cf246a68602908d
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-03-09 12:35:32 +0100

    Updated on 2024-03-09 12:35:31

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,2,09,12,25,24);
+timestamp = new Date(2024,2,09,12,35,26);
 data = [
 ['0205',1,2,32452409,'0040. TOURNER','1 x SIEGE CAMFLEX 2" FACT. 0,6',3,'',2,''],
 ['0205',1,2,32455931,'0020. BAGUE DE TEC-RING 78400 1"1/2.....','1 x BAGUE DE TEC-RING 78400 1"1/2',2.5,'',1,''],
