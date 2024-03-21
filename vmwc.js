6a10cf9244c1d0d5ea9e8e8d8395355baa20f595
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-03-21 03:40:53 +0100

    Updated on 2024-03-21 03:40:52

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,2,21,03,35,42);
+timestamp = new Date(2024,2,21,03,40,47);
 data = [
 ['0205',1,2,32412287,'0020. TOURNER','1 x TIGE DE DIAPHRAGME',0.45,'',42,''],
 ['0205',1,2,32443667,'0040. TOURNER (FINITION)','2 x SIEGE METALLIQUE 78400 2"',5.5,'',2,''],
