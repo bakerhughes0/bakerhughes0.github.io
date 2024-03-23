ccc0a5d5b23b9f2871a4488f758b0e510fc43fb5
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-03-23 06:55:31 +0100

    Updated on 2024-03-23 06:55:30

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,2,23,06,50,33);
+timestamp = new Date(2024,2,23,06,55,27);
 data = [
 ['0205',1,2,32412287,'0020. TOURNER','1 x TIGE DE DIAPHRAGME',0.45,'',44,''],
 ['0205',1,2,32336708,'0010. TOURNER','2 x USINAGE DE CLAPET 535H DE 0.75-2" CV',2,'',17,''],
