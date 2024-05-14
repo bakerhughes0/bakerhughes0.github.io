12b09f77da24378e35588794208437973a26574f
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-05-14 18:10:44 +0200

    Updated on 2024-05-14 18:10:44

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,4,14,18,05,49);
+timestamp = new Date(2024,4,14,18,10,34);
 data = [
 ['0205',1,2,32401831,'0040. REPRISE MISE A LONGUEUR','2 x BOITE A RONDELLE RESSORT',0.75,'',20,''],
 ['0205',1,2,32475085,'0020. TOURNER','1 x RONDELLE DE CDE MANUELLE',1.5,'',50,''],
