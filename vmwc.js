18bc9d6db72268a897bef22eca39b95e70afa8cd
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-07-08 16:25:41 +0200

    Updated on 2024-07-08 16:25:40

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,6,08,16,20,27);
+timestamp = new Date(2024,6,08,16,25,30);
 data = [
 ['0205',1,2,32543376,'0030. TOURNER (EBAUCHE)','1 x CAGE 47K 4 SPECIAL RET FI',2.75,'',6,''],
 ['0205',1,2,32521741,'0010. TOURNER','20 x PLAQUE DE DIAPHRAGME',2.35,'',34,''],
