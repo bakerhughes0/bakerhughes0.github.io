487177465d3f20edd34f78b3fcdd6b405c9238af
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-08-24 19:15:33 +0200

    Updated on 2024-08-24 19:15:32

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,7,24,19,10,23);
+timestamp = new Date(2024,7,24,19,15,23);
 data = [
 ['0205',1,2,32654297,'0020. TOURNER','2 x RONDELLE DE RETENUE',0.9,'',43,''],
 ['0205',1,2,32684348,'0030. TOURNER','1 x ADAPTEUR (côté vanne) MINITORK 6" ',0.85,'1683509-1000 ELECTRICITE DE FRANCE SA (E',23,''],
