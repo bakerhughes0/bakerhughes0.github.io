2b276b2afdb9c6e0aa9ae4bb48f9904d090609ea
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-10-06 18:45:32 +0200

    Updated on 2024-10-06 18:45:32

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,9,06,18,40,24);
+timestamp = new Date(2024,9,06,18,45,25);
 data = [
 ['0205',1,2,32654297,'0020. TOURNER','2 x RONDELLE DE RETENUE',0.9,'',86,''],
 ['0205',1,2,32688800,'0020.    TOURNER CAPUCHON','1 x CAPUCHON',3,'',27,''],
