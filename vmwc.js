63f454a93f59226acd93edfa5755a1caea1cbab4
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2024-10-29 12:40:37 +0100

    Updated on 2024-10-29 12:40:36

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2024,9,29,12,35,25);
+timestamp = new Date(2024,9,29,12,40,26);
 data = [
 ['0205',1,2,32654297,'0020. TOURNER','2 x RONDELLE DE RETENUE',0.9,'',109,''],
 ['0205',1,2,32719644,'0010. TOURNER (REUSINAGE)','2 x BAGUE SERRAGE 35K 6"(REUSINAGE',0.7,'',63,''],
