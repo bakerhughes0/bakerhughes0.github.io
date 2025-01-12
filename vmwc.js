1bdacf25331baf59f5226827eb8bc6ab26b4f3b0
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-01-12 06:30:34 +0100

    Updated on 2025-01-12 06:30:33

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,0,12,06,25,25);
+timestamp = new Date(2025,0,12,06,30,25);
 data = [
 ['0205',1,2,32654297,'0020. TOURNER','2 x RONDELLE DE RETENUE',0.9,'',184,''],
 ['0205',1,2,32736316,'0010. TOURNER','60 x PLAQUE DE DIAPHRAGME',6.35,'',87,''],
