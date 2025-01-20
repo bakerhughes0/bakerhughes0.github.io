93fdfed7ed171f790ed13b52fa77ab75fc85dd1c
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-01-20 19:40:37 +0100

    Updated on 2025-01-20 19:40:37

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,0,20,19,30,25);
+timestamp = new Date(2025,0,20,19,40,25);
 data = [
 ['0205',1,2,32654297,'0020. TOURNER','2 x RONDELLE DE RETENUE',0.9,'',192,''],
 ['0205',1,2,32630857,'0030. TOURNER (CONNEXION)','2 x COUVERCLE INF 87/88-6 1/2" NPT',2.4,'',111,''],
