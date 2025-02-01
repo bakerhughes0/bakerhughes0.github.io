7e9f54fdd78c9fa5f4d0fcbba142988d1f6f2a4b
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-02-01 06:10:34 +0100

    Updated on 2025-02-01 06:10:34

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,1,01,06,05,23);
+timestamp = new Date(2025,1,01,06,10,24);
 data = [
 ['0205',1,2,32630857,'0030. TOURNER (CONNEXION)','2 x COUVERCLE INF 87/88-6 1/2" NPT',2.4,'',123,''],
 ['0205',1,2,32918024,'0020. TOURNER CLAPET','2 x CLAPET 571 1" #A',3.7,'',10,''],
