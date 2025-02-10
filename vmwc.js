b8de78743668e3d4d0a6536a7a532a5ceee748ee
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-02-10 19:30:41 +0100

    Updated on 2025-02-10 19:30:41

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,1,10,19,20,25);
+timestamp = new Date(2025,1,10,19,30,27);
 data = [
 ['0205',1,3,32949828,'0020. TOURNER','1 x WASHER OD=40MM, ID=17MM',0.5,'',0,''],
 ['0205',1,2,32630857,'0030. TOURNER (CONNEXION)','2 x COUVERCLE INF 87/88-6 1/2" NPT',2.4,'',132,''],
