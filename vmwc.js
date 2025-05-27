a39a85ac70cadd2f4972dd95258b9515760c9e96
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-05-27 05:15:33 +0200

    Updated on 2025-05-27 05:15:32

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,4,27,05,10,24);
+timestamp = new Date(2025,4,27,05,15,24);
 data = [
 ['0205',1,2,32807369,'0010. TOURNER','50 x PLAQUE DE DIAPHRAGME',5.35,'',104,''],
 ['0205',1,2,33024153,'0010. TOURNER','4 x GOUJON 5/16" 18NC2A 40MM',0.42,'',33,''],
