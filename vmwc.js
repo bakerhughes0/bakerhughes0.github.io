0980ad5af87a1a9711313fc5159f5ba645ab2649
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-05-31 14:30:38 +0200

    Updated on 2025-05-31 14:30:37

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,4,31,14,20,26);
+timestamp = new Date(2025,4,31,14,30,30);
 data = [
 ['0205',1,2,32807369,'0010. TOURNER','50 x PLAQUE DE DIAPHRAGME',5.35,'',108,''],
 ['0205',1,2,33010184,'0030. TOURNER','1 x SIEGE CAMFLEX FACT 1',3,'',57,''],
