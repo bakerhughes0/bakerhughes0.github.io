ba40fcc2178deb55500c5c5a41e944621d03d03f
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-06-17 06:10:36 +0200

    Updated on 2025-06-17 06:10:36

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date(2025,5,17,06,05,29);
+timestamp = new Date(2025,5,17,06,10,28);
 data = [
 ['0205',1,2,33079305,'0020. TOURNER','6 x ENTRETOISE(SIEGE VPK-BS CV<0.1',0.9,'',33,''],
 ['0205',1,2,32958754,'0025. TOURNER','2 x GOUPILLE CANNELEE  DIA 5 MM',0.95,'',120,''],
