9c9a1669c5bcdc72a7a050dee1b670317e76aa6d
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-06-23 18:10:29 +0200

    Updated on 2025-06-23 18:10:27

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-06-23 18:05:29');
+timestamp = new Date('2025-06-23 18:10:26');
 data = [
 ["0205",1,2,32927524,"0073. TOURNAGE INTERMEDIAIRE","1 x SIEGE 21000 1\" 300LB",1.5,"1761835-1000 JIYOU ENERGY TECHNOLOGY CO, LIMITED",20,""],
 ["0205",1,2,32958754,"0025. TOURNER","2 x GOUPILLE CANNELEE  DIA 5 MM",0.95,"1755620-1000 PROCESS SYSTEMS",126,""],
@@ -2551,7 +2551,7 @@ data = [
 ["1008",1,2,33014505,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 120000DA2-3006N040 => 120000DA2-163S1063",0.09,"REWORK",90,"R"],
 ["1008",1,2,33014506,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 150000DA2-3006N915 => 150000DA2-163S1063",0.145,"REWORK",90,"R"],
 ["1008",1,2,33014507,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 075000DA2-3006N040 => 075000DA2-163S1063",0.14,"REWORK",90,"R"],
-["1008",1,2,33014511,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 090000DZ2-3006N915 => 090000DZ2-163S1063",0.097,"REWORK",89,"R"],
+["1008",1,2,33014511,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 090000DZ2-3006N915 => 090000DZ2-163S1063",0.097,"REWORK",90,"R"],
 ["1008",1,2,33014512,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 125000DZ2-3131N040 => 125000DZ2-226-1557",0.082,"REWORK",89,"R"],
 ["1008",1,2,33014504,"0010. Mise en conformit\u00e9 et effacer marquage","79 x 150000DA2-3006N417=> 150000DA2-163S1063",7.9,"REWORK",90,"R"],
 ["1008",1,2,33015838,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 035008104-594-A780 => 035008104-594-0000",0.1,"REWORK",89,"R"],
