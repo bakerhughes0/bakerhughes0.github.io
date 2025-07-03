ea559a4fd1d2bed2085f57292486fc8aabe2e4ed
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-07-03 18:35:23 +0200

    Updated on 2025-07-03 18:35:22

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-07-03 18:30:26');
+timestamp = new Date('2025-07-03 18:35:21');
 data = [
 ["0205",1,4,33019348,"0020. TOURNER GRAIN DE P-E VARIPACK..........","1 x GRAIN DE P.E.",2.0,"1772158-4000 GE OIL & GAS INDIA PRIVATE LIMITED",91,""],
 ["0205",1,2,33074398,"0020. TOURNER GRAIN DE PE","3 x GRAIN DE PRESSE-ETOUPE O-RING",4.25,"",50,""],
@@ -2160,7 +2160,7 @@ data = [
 ["1008",1,2,33014506,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 150000DA2-3006N915 => 150000DA2-163S1063",0.145,"REWORK",100,"R"],
 ["1008",1,2,33014507,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 075000DA2-3006N040 => 075000DA2-163S1063",0.14,"REWORK",100,"R"],
 ["1008",1,2,33014511,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 090000DZ2-3006N915 => 090000DZ2-163S1063",0.097,"REWORK",100,"R"],
-["1008",1,2,33014512,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 125000DZ2-3131N040 => 125000DZ2-226-1557",0.082,"REWORK",99,"R"],
+["1008",1,2,33014512,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 125000DZ2-3131N040 => 125000DZ2-226-1557",0.082,"REWORK",100,"R"],
 ["1008",1,2,33014504,"0010. Mise en conformit\u00e9 et effacer marquage","79 x 150000DA2-3006N417=> 150000DA2-163S1063",7.9,"REWORK",100,"R"],
 ["1008",1,2,33015838,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 035008104-594-A780 => 035008104-594-0000",0.1,"REWORK",99,"R"],
 ["1008",1,2,33016921,"0010. Mise en conformit\u00e9 et effacer marquage","5 x 000047333-104B0000 => 000047333-300A0000",0.5,"REWORK",99,"R"],
