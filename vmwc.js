c6aa688954df15d3de56183ea30281cb4b705cdc
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-07-26 18:00:22 +0200

    Updated on 2025-07-26 18:00:21

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-07-26 17:55:23');
+timestamp = new Date('2025-07-26 18:00:20');
 data = [
 ["0205",1,2,33052306,"0020. TOURNER CLAPET 78K","1 x CLAPET 78K 1 2500 6-STG TYP C UNBAL PREL",2.25,"",75,""],
 ["0205",1,2,32871624,"0082. TOURNER FINITION COTEE QUEUE","1 x PLUG #521 1\"1/2 TYPE E 40000 SERIES",2.0,"1573126-5000 FRAMATOME SPAIN SOCIEDAD LIMITADA.",3,""],
@@ -2003,7 +2003,7 @@ data = [
 ["1008",1,2,32995704,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 000047340-323-0000 => 000047340-300A0000",0.1,"REWORK",137,"R"],
 ["1008",1,2,33014505,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 120000DA2-3006N040 => 120000DA2-163S1063",0.09,"REWORK",123,"R"],
 ["1008",1,2,33014506,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 150000DA2-3006N915 => 150000DA2-163S1063",0.145,"REWORK",123,"R"],
-["1008",1,2,33014507,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 075000DA2-3006N040 => 075000DA2-163S1063",0.14,"REWORK",122,"R"],
+["1008",1,2,33014507,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 075000DA2-3006N040 => 075000DA2-163S1063",0.14,"REWORK",123,"R"],
 ["1008",1,2,33014511,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 090000DZ2-3006N915 => 090000DZ2-163S1063",0.097,"REWORK",122,"R"],
 ["1008",1,2,33014512,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 125000DZ2-3131N040 => 125000DZ2-226-1557",0.082,"REWORK",122,"R"],
 ["1008",1,2,33014504,"0010. Mise en conformit\u00e9 et effacer marquage","79 x 150000DA2-3006N417=> 150000DA2-163S1063",7.9,"REWORK",123,"R"],
