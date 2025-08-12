e692503caa7da73e2d9cfa50e2e302237914bbec
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-08-12 19:50:21 +0200

    Updated on 2025-08-12 19:50:20

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-08-12 19:45:20');
+timestamp = new Date('2025-08-12 19:50:19');
 data = [
 ["0205",1,2,33052306,"0020. TOURNER CLAPET 78K","1 x CLAPET 78K 1 2500 6-STG TYP C UNBAL PREL",2.25,"",92,""],
 ["0205",1,2,33121508,"0010. TOURNER","56 x PLAQUE DE DIAPHRAGME",5.95,"",27,""],
