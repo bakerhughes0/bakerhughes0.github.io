f97013c9be78c4a8190150f25500ae8be8ef89f8
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-08-21 18:35:24 +0200

    Updated on 2025-08-21 18:35:23

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-08-21 18:30:25');
+timestamp = new Date('2025-08-21 18:35:22');
 data = [
 ["0205",1,2,33052306,"0020. TOURNER CLAPET 78K","1 x CLAPET 78K 1 2500 6-STG TYP C UNBAL PREL",2.25,"",101,""],
 ["0205",1,2,33121508,"0010. TOURNER","56 x PLAQUE DE DIAPHRAGME",5.95,"",36,""],
@@ -2233,7 +2233,7 @@ data = [
 ["1008",1,2,33014506,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 150000DA2-3006N915 => 150000DA2-163S1063",0.145,"REWORK",149,"R"],
 ["1008",1,2,33014507,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 075000DA2-3006N040 => 075000DA2-163S1063",0.14,"REWORK",149,"R"],
 ["1008",1,2,33014511,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 090000DZ2-3006N915 => 090000DZ2-163S1063",0.097,"REWORK",149,"R"],
-["1008",1,2,33014512,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 125000DZ2-3131N040 => 125000DZ2-226-1557",0.082,"REWORK",148,"R"],
+["1008",1,2,33014512,"0010. Mise en conformit\u00e9 et effacer marquage","0 x 125000DZ2-3131N040 => 125000DZ2-226-1557",0.082,"REWORK",149,"R"],
 ["1008",1,2,33014504,"0010. Mise en conformit\u00e9 et effacer marquage","79 x 150000DA2-3006N417=> 150000DA2-163S1063",7.9,"REWORK",149,"R"],
 ["1008",1,2,33015838,"0010. Mise en conformit\u00e9 et effacer marquage","1 x 035008104-594-A780 => 035008104-594-0000",0.1,"REWORK",148,"R"],
 ["1008",1,2,33016921,"0010. Mise en conformit\u00e9 et effacer marquage","5 x 000047333-104B0000 => 000047333-300A0000",0.5,"REWORK",148,"R"],
