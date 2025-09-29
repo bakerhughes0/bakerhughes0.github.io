6bb22c8c7a54a22ce4a9634f973ca662df93fe1c
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-09-29 06:00:24 +0200

    Updated on 2025-09-29 06:00:23

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-09-29 05:50:25');
+timestamp = new Date('2025-09-29 06:00:21');
 data = [
 ["0205",1,2,33200315,"0010. TOURNER","10 x PLAQUE DE DIAPHRAGME",1.35,"",24,""],
 ["0205",1,2,33060291,"0020. TOURNER CAPUCHON 1\" HEXAGONAL","1 x CAPUCHON",2.25,"",153,""],
