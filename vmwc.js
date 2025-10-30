a8c8a33810d5d0b508f3ca466f2276510dbcfa51
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2025-10-30 05:40:29 +0100

    Updated on 2025-10-30 05:40:27

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2025-10-30 05:30:19');
+timestamp = new Date('2025-10-30 05:40:26');
 data = [
 ["0205",1,2,33060291,"0020. TOURNER CAPUCHON 1\" HEXAGONAL","1 x CAPUCHON",2.25,"",184,""],
 ["0205",1,2,33149237,"0030. TOURNER AV.FRAISAGE","1 x CAGE 3/4-1\" 600LB 21KRC LIN",3.5,"",50,""],
