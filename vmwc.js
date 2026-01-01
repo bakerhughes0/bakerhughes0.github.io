ea41e6057c63be4805780002c6758d102299d973
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2026-01-01 06:20:21 +0100

    Updated on 2026-01-01 06:20:19

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2026-01-01 06:10:20');
+timestamp = new Date('2026-01-01 06:20:18');
 data = [
 ["0205",1,2,33397806,"0030. TOURNER (EB CLAPET)","1 x CLAPET S/A 41000 2\"",2.35,"",17,""],
 ["0205",1,2,33393198,"0020. TOURNER","1 x RONDELLE",0.5,"1857660-4000 GE OIL & GAS INDIA PRIVATE LIMITED",31,""],
