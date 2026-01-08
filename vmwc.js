63122f8c7fa3d05155ae2587f7868ef4b1bd0411
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2026-01-08 07:30:21 +0100

    Updated on 2026-01-08 07:30:20

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2026-01-08 07:20:23');
+timestamp = new Date('2026-01-08 07:30:19');
 data = [
 ["0205",1,2,33397806,"0030. TOURNER (EB CLAPET)","1 x CLAPET S/A 41000 2\"",2.35,"",24,""],
 ["0205",1,2,33393198,"0020. TOURNER","1 x RONDELLE",0.5,"1853569-2000 CRYOSTAR SAS",38,""],
