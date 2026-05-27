600b6beeaa85c2e18954c0e15a035599bf7b4212
bakerhughes0 <113053698+bakerhughes0@users.noreply.github.com>
2026-05-27 05:20:34 +0200

    Updated on 2026-05-27 05:20:32

--- vmwc.js
+++ vmwc.js
@@ -1,4 +1,4 @@
-timestamp = new Date('2026-05-27 05:10:34');
+timestamp = new Date('2026-05-27 05:20:31');
 data = [
 ["0205",1,2,33644435,"0020. TOURNER EBAUCHE AVANT STELLITE","1 x BUSHING LINER 18.0 ID",1.5,"1841595-13000 Dresser Machinery(Suzhou) Co.Ltd.",5,"",1,1,"N"],
 ["0205",1,2,33602900,"0030. TOURNER","4 x CONNEXION 1/2\"NPT",1.7,"1884082-1000 ARKEMA FRANCE",35,"",0,0,"N"],
